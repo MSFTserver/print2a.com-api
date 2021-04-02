784752b20549816b120ef2a1e68c7e4047c56d1e
MSFTserver <msftserver@gmail.com>
2021-04-02 00:18:23 -0700

    update for file downloading

--- src/readOperations.js
+++ src/readOperations.js
@@ -1,13 +1,16 @@
 import { promises as fs } from "fs";
+import zipFolder from "zip-folder";
 import path from "path";
 import { getUserName, modeToOctal, sizeToHuman } from "./util.js";
 
 export default async (req, res) => {
   // The default starting path is the directory from which server is executed.
   // Otherwise, get the full path keyed under unnamed Express parameter '0'.
-  const requestedFilePath =
-    "../../repo/" + req.params["0"] || "../../repo/";
-
+  const print2aApiUrl = "https://print2a.com:5757"
+  let requestedPath = `../../repo/${req.params[0]}` || `../../repo/`;
+  if (req.url.startsWith("/DLZIP")){
+    requestedPath = `../../${req.params[0].replace(/\//g,"+").replace("+","/")}`
+  }
   // getDirectories
   //
   // read directory from given path.
@@ -27,10 +30,10 @@ export default async (req, res) => {
   //
 
   const handleDirectoryRequest = async () => {
-    const dirEntries = await fs.readdir(requestedFilePath);
+    const dirEntries = await fs.readdir(requestedPath);
     const nodes = await Promise.all(
       dirEntries.map(async node => {
-        const path = `${requestedFilePath}/${node}`;
+        const path = `${requestedPath}/${node}`;
         const nodeStats = await fs.stat(path);
         let childrenCount = 0;
         if (nodeStats.isDirectory()) {
@@ -48,7 +51,7 @@ export default async (req, res) => {
           birthtime: nodeStats.birthtime,
           mtime: nodeStats.mtime,
           childrenCount: childrenCount,
-          path: requestedFilePath
+          path: requestedPath
         };
       })
     );
@@ -61,24 +64,59 @@ export default async (req, res) => {
   // Return a single file, define filename in Content-Disposition header
   // https://expressjs.com/en/api.html#res.download
   const handleFileRequest = async () => {
-    const fileName = path.basename(requestedFilePath);
-    res.download(requestedFilePath, fileName);
+    const fileName = path.basename(requestedPath);
+    res.download(requestedPath, fileName);
+  };
+
+  // handleFolderRequest
+  //
+  // Return zipped up folder, define foldername in Content-Disposition header
+  // https://expressjs.com/en/api.html#res.download
+  const handleFolderRequest = async () => {
+    const folderName = path.basename(requestedPath);
+    console.log(folderName)
+    console.log(`../../repo/${req.params[0]}`);
+    let folderContents = await fs.readdir(`../../DLZIP/`)
+    console.log(folderContents.includes(`${req.params[0].replace(/\//g,"+")}.zip`))
+    if (!folderContents.includes(`${req.params[0].replace(/\//g,"+")}.zip`)) {
+      zipFolder(`../../repo/${req.params[0]}`, `../../DLZIP/${req.params[0].replace(/\//g,"+")}.zip`, function(err) {
+          if(err) {
+              console.log('oh no!', err);
+              res.json({status:`ERROR`,msg: err})
+          } else {
+              res.json({
+                status:`COMPLETE`,
+                link:`${print2aApiUrl}/DLZIP/${req.params[0].replace(/\//g,"+")}.zip`,
+                fileName: `${req.params[0].replace(/\//g,"+")}.zip`
+              })
+          }
+      });
+    } else {
+      res.json({
+        status:`COMPLETE`,
+        link:`${print2aApiUrl}/DLZIP/${req.params[0].replace(/\//g,"+")}.zip`,
+        fileName: `${req.params[0].replace(/\//g,"+")}.zip`
+      })
+    }
   };
 
   // Check if the path is accessible.
   // Then return the directory, file or 404.
   //
-  fs.access(requestedFilePath)
+  fs.access(requestedPath)
     .then(async () => {
-      const requestStats = await fs.stat(requestedFilePath);
+      const requestStats = await fs.stat(requestedPath);
       const isRequestingDirectory = requestStats.isDirectory();
-      if (isRequestingDirectory) {
+      const isRequestingFolder = req.headers.request;
+      if (isRequestingDirectory && !isRequestingFolder) {
         handleDirectoryRequest();
-      } else {
+      } else if (!isRequestingDirectory && !isRequestingFolder) {
         handleFileRequest();
+      } else if (isRequestingDirectory && isRequestingFolder){
+        handleFolderRequest()
       }
     })
     .catch(() => {
-      res.status(404).send("404 Not found<br>"+requestedFilePath)
+      res.status(404).send("404 Not found<br>"+requestedPath)
     });
 };

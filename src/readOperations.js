f66c6a25e561b5d02e3b3ca6260a73a79b4b4d2f
MSFTserver <MSFTserver@gmail.com>
2022-05-30 07:55:11 -0700

    test json

--- src/readOperations.js
+++ src/readOperations.js
@@ -126,7 +126,7 @@ export default async (req, res) => {
       res.set('Content-Type', 'text/plain');
       res.send(textContent.toString());
     } else {
-      console.log(textContent.toString())
+      console.log(textContent.toJSON())
       res.send(textContent.toString());
     }
   }

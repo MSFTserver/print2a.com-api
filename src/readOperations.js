bb1fbf17c1b9937b8e39a59d74d3b1d029726135
MSFTserver <MSFTserver@gmail.com>
2022-05-30 07:49:19 -0700

    to string log

--- src/readOperations.js
+++ src/readOperations.js
@@ -126,7 +126,7 @@ export default async (req, res) => {
       res.set('Content-Type', 'text/plain');
       res.send(textContent.toString());
     } else {
-      console.log(textContent)
+      console.log(textContent.toString())
       res.send(textContent.toString());
     }
   }

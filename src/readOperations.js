b6290158e3c0fc81097dfbabe03d095ad6238988
MSFTserver <MSFTserver@gmail.com>
2022-06-08 23:46:57 -0700

    Update readOperations.js

--- src/readOperations.js
+++ src/readOperations.js
@@ -125,7 +125,7 @@ export default async (req, res) => {
       } else if (['stl', 'obj'].includes(fileExt)){
         res.set('Contnet-Type', 'application/json');
         let objectBuffer = new Uint16Array(fileContent);
-        res.send(fileContent);
+        res.send(objectBuffer);
       } else {
         res.send(fileContent.toString());
       }
